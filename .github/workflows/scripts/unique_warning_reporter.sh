a3c3d0f33745bf0b488f0ad61ceb3883f603a915
runora95 <runor.agbaire@xanadu.ai>
2025-11-25 10:50:06 -0500

    Updating warning as errors

--- .github/workflows/scripts/unique_warning_reporter.sh
+++ .github/workflows/scripts/unique_warning_reporter.sh
@@ -4,10 +4,10 @@
 # warnings-as-errors action runs. The volume of each reported warning can be used to indicate
 # the severity or importance of rectification.
 
-JOBID=$(gh run list -w "Test-suite with Python warnings as errors" -L 1 --json databaseId -q '.[0].databaseId')
+JOBID=$(gh run list -w "Test-suite with Python warnings as errors" --repo PennylaneAI/pennylane -L 1 --json databaseId -q '.[0].databaseId')
 echo "View latest job at https://github.com/PennyLaneAI/pennylane/actions/runs/$JOBID"
-gh run view $JOBID --log-failed >/tmp/job_$JOBID.out
-cat /tmp/job_$JOBID.out | grep "Warning:" | awk '{split($0,a,"Warning:"); print a[1]"Warning"}' | awk '{split($0,a," - "); print a[2]}' | sort -u >unique_wae.txt
+gh run view $JOBID --log-failed --repo PennylaneAI/pennylane >/tmp/job_$JOBID.out
+cat /tmp/job_$JOBID.out | grep "Warning:" | awk '{split($0,a,"Warning:"); print a[1]"Warning"}' | awk '{split($0,a," "); print a[3]}' | sort -u >unique_wae.txt
 
 declare -A waeCounts
 
