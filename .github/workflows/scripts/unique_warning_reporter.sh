5186e4e82b476324667112f97745abc5e2c1fee7
runora95 <runor.agbaire@xanadu.ai>
2025-12-04 10:44:28 -0500

    dynamic warnings

--- .github/workflows/scripts/unique_warning_reporter.sh
+++ .github/workflows/scripts/unique_warning_reporter.sh
@@ -8,30 +8,17 @@ JOBID=$(gh run list -w "Test-suite with Python warnings as errors" --repo Pennyl
 echo "View latest job at https://github.com/PennyLaneAI/pennylane/actions/runs/$JOBID"
 gh run view $JOBID --log-failed --repo PennylaneAI/pennylane >/tmp/job_$JOBID.out
 
-cat <<EOF > unique_wae.txt
-FutureWarning
-pennylane.exceptions.PennyLaneDeprecationWarning
-pytest.PytestCollectionWarning
-RuntimeWarning
-numpy.exceptions.ComplexWarning
-pytest.PytestUnraisableExceptionWarning
-PendingDeprecationWarning
-pytest.PytestRemovedIn9Warning
-UserWarning
-DeprecationWarning
-EOF
-
-declare -A waeCounts
-
-while read -r line; do
-    [[ -n "$line" && "$line" != [[:blank:]]* ]] && waeCounts["$line"]=$(grep -F -c "$line" "/tmp/job_$JOBID.out")
-done <unique_wae.txt
-echo $waeCounts
+grep -oE ' [A-Za-z0-9_.]+Warning:' /tmp/job_$JOBID.out | \
+    sed 's/^ //' | \
+    sed 's/:$//' | \
+    sort | \
+    uniq -c | \
+    awk '{print $2, $1}' > unique_wae.txt
 
 jsonOut="[{"
-for x in "${!waeCounts[@]}"; do
-    echo $x :: ${waeCounts[$x]}
-    jsonOut+="\"$x\":${waeCounts[$x]},"
-done
+while read -r name count; do
+    echo "$name :: $count"
+    jsonOut+="\"$name\":$count,"
+done < unique_wae.txt
 jsonOut="${jsonOut::-1}}]"
 echo $jsonOut >unique_wae.json
